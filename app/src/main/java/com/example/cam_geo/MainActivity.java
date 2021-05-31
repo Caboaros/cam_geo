dd49e4a4cf1d9fbbfd3f8d77241b1a9e9b32d720
CelsoPontes <celsocomcarinho@gmail.com>
2021-05-31 20:17:44 -0300

    +GPStracker +app funcional -ainda não pede permissão no início

--- app/src/main/java/com/example/cam_geo/MainActivity.java
+++ app/src/main/java/com/example/cam_geo/MainActivity.java
@@ -1,14 +1,97 @@
 package com.example.cam_geo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Location;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageViewPhoto = (ImageView) findViewById(R.id.img_photo);
+        requestPermissions();
+        configGPSButton();
+        configCamButton();
+    }
+
+    private void requestPermissions() {
+        askAboutCamera(); //using EasyPermissions
+//        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 0);
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+//          not working
+//        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+//        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+//            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 0);
+//        }
+    }
+
+    private void configCamButton() {
+        findViewById(R.id.btn_cam).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+            }
+        });
+    }
+
+    private void configGPSButton() {
+//        btn_geo = (Button) findViewById(R.id.btn_gps);
+        findViewById(R.id.btn_gps).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GPStracker g = new GPStracker(getApplication());
+                Location l = g.getLocation();
+                if(l != null) {
+                    double lat = l.getLatitude();
+                    double lon = l.getLongitude();
+                    Toast.makeText(getApplicationContext(),
+                            "LATITUDE: " + lat + "\n LONGITUDE: " + lon,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+//    bundle para pegar a foto e colocar na ImageView
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            imageViewPhoto.setImageBitmap(photo);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void askAboutCamera(){
+        EasyPermissions.requestPermissions(
+                this,
+                "A partir deste ponto a permissão de câmera é necessária.",
+                0,
+                Manifest.permission.CAMERA );
     }
 }
